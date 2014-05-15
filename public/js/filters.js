81c82e672348a1bea8e4be77d5dbc684e9afd0a8
pauder <paul.derosanbo@cantico.fr>
2014-05-15 16:33:50 +0200

    requirejs integration

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,10 +1,12 @@
-'use strict';
+define(['angular', 'services'], function (angular, services) {
+	'use strict';
 
-/* Filters */
-
-angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    };
-  }]);
+	/* Filters */
+  
+	angular.module('myApp.filters', ['myApp.services'])
+		.filter('interpolate', ['version', function(version) {
+			return function(text) {
+				return String(text).replace(/\%VERSION\%/mg, version);
+			};
+	}]);
+});
