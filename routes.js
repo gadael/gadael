0b50b124e873a75dd71224c7a88f5f97110ca985
polo2ro <paul@rosanbo.com>
2014-05-15 15:13:13 +0200

    angular-seed & drywall

---
+++ routes.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.set('X-Auth-Required', 'true');
+  req.session.returnUrl = req.originalUrl;
+  res.redirect('/login/');
+}
+
+function ensureAdmin(req, res, next) {
+  if (req.user.canPlayRoleOf('admin')) {
+    return next();
+  }
+  res.redirect('/');
+}
+
+function ensureAccount(req, res, next) {
+  if (req.user.canPlayRoleOf('account')) {
+    if (req.app.config.requireAccountVerification) {
+      if (req.user.roles.account.isVerified !== 'yes' && !/^\/account\/verification\//.test(req.url)) {
+        return res.redirect('/account/verification/');
+      }
+    }
+    return next();
+  }
+  res.redirect('/');
+}
+
+exports = module.exports = function(app) {
+	//front end use the index.html and the partials folder
+	
+	// rest api used by angular
+	app.get('/rest/accounts', require('./rest/Account').getAccounts);
+	app.get('/rest/account', require('./rest/Account').getAccount);
+
+	//route not found
+	//app.all('*', require('./rest').http404);
+};
