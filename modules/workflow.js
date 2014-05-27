52bf455d18da1bbda6e6361a1c8475dd911aa809
pauder <paul.derosanbo@cantico.fr>
2014-05-27 23:12:31 +0200

    add gettext server side

---
+++ modules/workflow.js
@@ -0,0 +1,29 @@
+'use strict';
+
+exports = module.exports = function(req, res) {
+  var workflow = new (require('events').EventEmitter)();
+  
+  workflow.httpstatus = 200;
+
+  workflow.outcome = {
+    success: false,
+    errors: [],
+    errfor: {}
+  };
+
+  workflow.hasErrors = function() {
+    return Object.keys(workflow.outcome.errfor).length !== 0 || workflow.outcome.errors.length !== 0;
+  };
+
+  workflow.on('exception', function(err) {
+    workflow.outcome.errors.push('Exception: '+ err);
+    return workflow.emit('response');
+  });
+
+  workflow.on('response', function() {
+    workflow.outcome.success = !workflow.hasErrors();
+    res.status(workflow.httpstatus).send(workflow.outcome);
+  });
+
+  return workflow;
+};
