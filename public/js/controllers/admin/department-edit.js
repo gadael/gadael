3677c3782869bcb1df825ca64dc5cc7f5e3ca01b
polo2ro <paul@rosanbo.com>
2014-07-07 23:17:36 +0200

    departments

---
+++ public/js/controllers/admin/department-edit.js
@@ -0,0 +1,17 @@
+define([], function() {
+
+	return ['$scope', '$location', 'IngaResource', function($scope, $location, IngaResource) {
+
+		$scope.department = IngaResource('rest/admin/departments').loadRouteId();
+
+		$scope.back = function() {
+			$location.path('/admin/departments');
+		}
+		
+		$scope.saveDepartment = function() {
+			$scope.department.ingaSave();
+			$scope.back();
+	    }
+	}];
+});
+
