296e682109a62e1e8b2ea16ea0bc63370b9b1d8a
polo2ro <paul@rosanbo.com>
2014-07-19 11:12:58 +0200

    types settings

---
+++ public/js/controllers/admin/type-edit.js
@@ -0,0 +1,16 @@
+define([], function() {
+
+	return ['$scope', '$location', 'IngaResource', function($scope, $location, IngaResource) {
+
+		$scope.type = IngaResource('rest/admin/types').loadRouteId();
+
+		$scope.back = function() {
+			$location.path('/admin/types');
+		}
+		
+		$scope.saveType = function() {
+			$scope.type.ingaSave($scope.back);
+	    }
+	}];
+});
+
---
+++ public/js/controllers/user/settings.js
@@ -0,0 +1,14 @@
+define([], function() {
+	return ['$scope', '$location', 'IngaResource', function($scope, $location, IngaResource) {
+		
+		$scope.user = IngaResource('rest/user/settings').loadRouteId();
+
+		$scope.back = function() {
+			// TODO: go to a user homepage
+		}
+		
+		$scope.saveUser = function() {
+			$scope.user.ingaSave($scope.back);
+	    }
+	}];
+});
