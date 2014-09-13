f817c3e856d4450e7a32afd801c6a78c4010e97e
polo2ro <paul@rosanbo.com>
2014-09-13 15:48:36 +0200

    new test file

---
+++ test/server/rest/admin/typesSpec.js
@@ -0,0 +1,35 @@
+'use strict';
+
+describe('vacations types admin rest service', function() {
+    
+    var mockServer = require('../mockServer')();
+    
+    var server;
+    var app;
+
+    /*
+    it('create the mock server', function(done) {
+        server = new mockServer(function(mockApp) {
+            expect(mockApp).toBeDefined();
+            app = mockApp;
+            done();
+        });
+    });
+    
+    
+    it('request types list as anonymous', function(done) {
+        server.get('/rest/admin/types', function(res) {
+            expect(res.statusCode).toEqual(401);
+            done();
+        });
+    });
+    
+    
+    
+    it('close the mock server', function(done) {
+        server.close(function() {
+            done();
+        });
+    });
+    */
+});
