50aaaa030f85d7831ff0bd81fea34a67d524869c
polo2ro <paul@rosanbo.com>
2014-10-02 22:53:33 +0200

    add the collections service

--- api/services/admin/users/delete.js
+++ api/services/admin/users/delete.js
@@ -6,9 +6,9 @@ exports = module.exports = function(services, app) {
     var service = new services.delete(app);
     
     /**
-     * Call the users get service
+     * Call the users delete service
      * 
-     * @param {Object} params
+     * @param {int} id      Document mongoose ID
      * @return {Promise}
      */
     service.call = function(id) {
