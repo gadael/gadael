1f28c0fa4241501f9b25ec3f7cbc694363f63f54
polo2ro <paul@rosanbo.com>
2014-11-01 23:11:21 +0100

    new service

---
+++ public/js/services/saveBeneficiaries.js
@@ -0,0 +1,49 @@
+define([], function() {
+    
+    /**
+     * Save one row
+     * @param   {$resource}   resource
+     * @returns {Promise}
+     */
+    var saveRow = function($q, resource) {
+        
+        var deferred = $q.defer();
+        
+        if (resource._id) {
+
+            resource.$save(function(data) {
+                deferred.resolve(data);
+            });
+            
+        } else {
+            resource.$create(function(data) {
+                deferred.resolve(data);
+            });
+        }
+
+        return deferred.promise;
+    };
+    
+    
+    
+    /**
+     * Save all beneficiaries
+     * 
+     */
+    return function($scope, collectionId, $q, catchOutcome) {
+
+        var promises = [];
+
+        $scope.collectionRights.forEach(function(resource) {
+            
+            resource.document = collectionId;
+            resource.ref = 'RightCollection';
+
+            promises.push(catchOutcome(saveRow($q, resource)));
+        });
+
+        var promise = $q.all(promises);
+
+        return promise;
+    };
+});
