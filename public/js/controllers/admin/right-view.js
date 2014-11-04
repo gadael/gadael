1a6464a7d1e0bf49b96c3e071a00befb7035e6c6
polo2ro <paul@rosanbo.com>
2014-11-04 22:46:44 +0100

    right view

--- public/js/controllers/admin/right-view.js
+++ public/js/controllers/admin/right-view.js
@@ -12,10 +12,21 @@ define([], function() {
 			$resource,
             $http
 		) {
+                
+                
+        var rightRule = $resource('rest/admin/rightrules/');
+        var rightRenewal = $resource('rest/admin/rightrenewals/');
 
 		$scope.right = IngaResource('rest/admin/rights').loadRouteId();
+                
         
-		
+        $scope.right.$promise.then(function() {
+            $scope.rightRules = rightRule.query({ right: $scope.right._id });
+            $scope.rightRenewals = rightRenewal.query({ right: $scope.right._id });
+        });
+        
+        
+        
 		
 		$scope.cancel = function() {
 			$location.path('/admin/rights');
