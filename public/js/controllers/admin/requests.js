f97b4e1f271427d7bbdecdceca91f195821ed1bc
polo2ro <paul@rosanbo.com>
2014-11-24 01:42:59 +0100

    add a request, start the process by selecting a user, creation of the popover

--- public/js/controllers/admin/requests.js
+++ public/js/controllers/admin/requests.js
@@ -2,9 +2,23 @@ define([], function() {
     
     'use strict';
     
-	return ['$scope', function($scope) {
+	return ['$scope', '$location', 'Rest', function($scope, $location, Rest) {
 
-		
-		
+        var users = Rest.admin.users.getResource();
+        
+        $scope.popover = {
+            selectuser: {
+                search: ''   
+            }
+        };
+        
+        
+        $scope.$watch('popover.selectuser.search', function(newValue, oldValue) {
+            $scope.popover.selectuser.users = users.query({ name: newValue });
+        });
+        
+        $scope.popoverSelect = function(user) {
+            $location.path('/admin/users/'+user._id);
+        };
 	}];
 });
