f7a70aaec4af57c6068723f27940ece1ef980882
polo2ro <paul@rosanbo.com>
2014-12-16 23:31:45 +0100

    add new controller to get the current logged in user

---
+++ rest/user/user.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var ctrlFactory = require('../controller');
+
+
+function getController() {
+    ctrlFactory.get.call(this, '/rest/user');
+    var ctrl = this;
+
+    this.controllerAction = function() {
+
+        var gt = ctrl.req.app.utility.gettext;
+
+        if (!ctrl.req.user) {
+            return ctrl.accessDenied(gt.gettext('You must be logged in'));
+        }
+
+        ctrl.res.status(200).json(ctrl.req.user);
+    };
+}
+getController.prototype = new ctrlFactory.get();
+
+
+
+exports = module.exports = {
+    get: getController
+};
