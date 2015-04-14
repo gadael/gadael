e467803e7f64819647acb6c4c241291d99e917eb
Paul de Rosanbo <paul@rosanbo.com>
2015-04-14 08:59:52 +0200

    rest

--- rest/account/collection.js
+++ rest/account/collection.js
@@ -6,13 +6,18 @@ var ctrlFactory = require('restitute').controller;
  * Get the collection for a user account
  * according to a date interval
  * This is collection object to use when creating a vacation request
+ *
+ * rest service parameters are:
+ *  dtstart
+ *  dtend
+ *
  */
 function getController() {
     ctrlFactory.get.call(this, '/rest/account/collection');
     var ctrl = this;
 
     this.controllerAction = function() {
-
+        ctrl.jsonService(this.service('account/collection/get', { user: this.req.user._id }));
     };
 }
 getController.prototype = new ctrlFactory.get();
