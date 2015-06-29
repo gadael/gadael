a6919e1c6eb39b10b165ce75ad9f63a2b48bb746
Paul de Rosanbo <paul@rosanbo.com>
2015-06-29 08:58:14 +0200

    Add the admin waiting requests controller

---
+++ rest/admin/waitingrequests.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var ctrlFactory = require('restitute').controller;
+
+
+
+function listController() {
+    ctrlFactory.list.call(this, '/rest/admin/waitingrequests');
+
+    this.controllerAction = function() {
+        this.jsonService(this.service('manager/waitingrequests/list'));
+    };
+}
+listController.prototype = new ctrlFactory.list();
+
+
+function getController() {
+    ctrlFactory.get.call(this, '/rest/admin/waitingrequests/:id');
+
+    this.controllerAction = function() {
+        this.jsonService(this.service('manager/waitingrequests/get'));
+    };
+}
+getController.prototype = new ctrlFactory.get();
+
+
+/**
+ * Confirm or reject a waiting request instead of a manager
+ */
+function updateController() {
+    ctrlFactory.update.call(this, '/rest/admin/waitingrequests/:id');
+
+    var controller = this;
+    this.controllerAction = function() {
+        controller.jsonService(
+            controller.service('manager/waitingrequests/save')
+        );
+    };
+}
+updateController.prototype = new ctrlFactory.update();
+
+
+
+exports = module.exports = {
+    list: listController,
+    get: getController,
+    update: updateController
+};
