3197b6e7eed218d437621ed3226f77afbd55eacc
Paul de Rosanbo <paul@rosanbo.com>
2015-08-06 22:10:29 +0200

    select the current approval step

--- public/js/controllers/manager/waitingrequest.js
+++ public/js/controllers/manager/waitingrequest.js
@@ -4,15 +4,36 @@ define([], function() {
 	return ['$scope',
 		'$location',
 		'Rest',
-        'getRequestStat', function(
+        'getRequestStat',
+        '$alert', function(
 			$scope,
 			$location,
 			Rest,
-            getRequestStat
+            getRequestStat,
+            $alert
 		) {
 
 
 		$scope.request = Rest.manager.waitingrequests.getFromUrl().loadRouteId();
+
+        $scope.request.$promise.then(function() {
+            // find the approval step to approve
+
+            $scope.request.approvalSteps.forEach(function(step) {
+                if ('waiting' === step.status && step.approvers.indexOf($scope.user._id)) {
+                    $scope.request.approvalStep = step;
+                }
+            });
+
+            if (undefined === $scope.request.approvalStep) {
+                $alert({
+                    title: 'No waiting step to approve',
+                    placement: 'top',
+                    type: 'danger',
+                    show: true
+                });
+            }
+        });
 
         $scope.stat = getRequestStat($scope.request);
 
@@ -22,11 +43,13 @@ define([], function() {
 
 
         $scope.accept = function() {
-
+            $scope.request.action = 'wf_accept';
+            $scope.request.ingaSave($scope.backToList);
         };
 
         $scope.reject = function() {
-
+            $scope.request.action = 'wf_reject';
+            $scope.request.ingaSave($scope.backToList);
         };
 
 
