1814487b0280404265efce3fa61a0e6c8d746fff
Paul de Rosanbo <paul@rosanbo.com>
2015-09-11 21:13:44 +0200

    round on numbers

--- public/js/controllers/admin/beneficiary-edit.js
+++ public/js/controllers/admin/beneficiary-edit.js
@@ -3,132 +3,7 @@ define(['q', 'async'], function(Q, async) {
     'use strict';
 
 
-    /**
-     * Create graph values
-     *
-     * @param {array} renewals
-     * @param {Promise} requestsPromise
-     * @param {function} next
-     *
-     *
-     */
-    function createGraphValues(renewals, requestsPromise, next)
-    {
-        var history = [];
 
-        /**
-         * renewals and adjustements by date
-         */
-        function buildRenewalsWithAdjustments() {
-
-            var deferred = Q.defer();
-            var lastRenewalId = renewals[renewals.length-1]._id;
-
-            renewals.forEach(function(r) {
-
-
-                history.push({
-                    position: r.start,
-                    add: r.initial_quantity
-                });
-
-                // process monthly update adjustments
-
-                r.adjustments.forEach(function(adjustment) {
-                    history.push({
-                        position: adjustment.from,
-                        add: adjustment.quantity
-                    });
-                });
-
-                // process manual adjustments
-
-                r.adjustmentPromise.then(function(adjustments) {
-                    adjustments.forEach(function(adjustment) {
-                        history.push({
-                            position: adjustment.timeCreated,
-                            add: adjustment.quantity
-                        });
-                    });
-
-                    if (0 === adjustments.length || adjustments[0].rightRenewal === lastRenewalId) {
-                        deferred.resolve();
-                    }
-                });
-            });
-
-            return deferred.promise;
-        }
-
-
-        function getElem(request)
-        {
-            if (undefined === request.absence || undefined === request.absence.distribution) {
-                return null;
-            }
-
-            var elem;
-            for(var i=0; i<request.absence.distribution.length; i++) {
-                elem = request.absence.distribution[i];
-                if (elem.right.id === renewals[0].right._id) {
-                    return elem;
-                }
-            }
-
-            return null;
-        }
-
-
-        /**
-         * requests by date
-         */
-        function buildRequests() {
-            var deferred = Q.defer();
-
-            requestsPromise.then(function(requests) {
-                requests.forEach(function(r) {
-                    var elem = getElem(r);
-                    if (null === elem) {
-                        return;
-                    }
-
-                    history.push({
-                        position: r.timeCreated,
-                        add: (-1 * elem.consumedQuantity)
-                    });
-                });
-
-                deferred.resolve();
-            });
-
-            return deferred.promise;
-        }
-
-
-
-
-        Q.all([buildRenewalsWithAdjustments(), buildRequests()]).then(function() {
-
-            history.sort(function (a, b) {
-                if (a.position > b.position) {
-                    return 1;
-                }
-                if (a.position < b.position) {
-                    return -1;
-                }
-                return 0;
-            });
-
-            var graph = [];
-            var current_quantity = 0;
-            history.forEach(function(element) {
-                current_quantity += element.add;
-                graph.push([element.position, current_quantity]);
-            });
-
-            next(graph);
-        });
-    }
 
 
 
@@ -143,7 +18,146 @@ define(['q', 'async'], function(Q, async) {
         '$modal',
         'catchOutcome',
         'gettext',
-        function($scope, $location, $routeParams, Rest, $modal, catchOutcome, gettext) {
+        'decimalAdjust',
+        function($scope, $location, $routeParams, Rest, $modal, catchOutcome, gettext, decimalAdjust) {
+
+
+
+
+
+        /**
+         * Create graph values
+         *
+         * @param {array} renewals
+         * @param {Promise} requestsPromise
+         * @param {function} next
+         *
+         *
+         */
+        function createGraphValues(renewals, requestsPromise, next)
+        {
+            var history = [];
+
+            function round(x) {
+                return decimalAdjust('round', x, -1);
+            }
+
+            /**
+             * renewals and adjustements by date
+             */
+            function buildRenewalsWithAdjustments() {
+
+                var deferred = Q.defer();
+                var lastRenewalId = renewals[renewals.length-1]._id;
+
+                renewals.forEach(function(r) {
+
+
+                    history.push({
+                        position: r.start,
+                        add: round(r.initial_quantity)
+                    });
+
+                    // process monthly update adjustments
+
+                    r.adjustments.forEach(function(adjustment) {
+                        history.push({
+                            position: adjustment.from,
+                            add: round(adjustment.quantity)
+                        });
+                    });
+
+                    // process manual adjustments
+
+                    r.adjustmentPromise.then(function(adjustments) {
+                        adjustments.forEach(function(adjustment) {
+                            history.push({
+                                position: adjustment.timeCreated,
+                                add: round(adjustment.quantity)
+                            });
+                        });
+
+                        if (0 === adjustments.length || adjustments[0].rightRenewal === lastRenewalId) {
+                            deferred.resolve();
+                        }
+                    });
+                });
+
+                return deferred.promise;
+            }
+
+
+            function getElem(request)
+            {
+                if (undefined === request.absence || undefined === request.absence.distribution) {
+                    return null;
+                }
+
+                var elem;
+                for(var i=0; i<request.absence.distribution.length; i++) {
+                    elem = request.absence.distribution[i];
+                    if (elem.right.id === renewals[0].right._id) {
+                        return elem;
+                    }
+                }
+
+                return null;
+            }
+
+
+            /**
+             * requests by date
+             */
+            function buildRequests() {
+                var deferred = Q.defer();
+
+                requestsPromise.then(function(requests) {
+                    requests.forEach(function(r) {
+                        var elem = getElem(r);
+                        if (null === elem) {
+                            return;
+                        }
+
+                        history.push({
+                            position: r.timeCreated,
+                            add: (-1 * elem.consumedQuantity)
+                        });
+                    });
+
+                    deferred.resolve();
+                });
+
+                return deferred.promise;
+            }
+
+
+
+
+            Q.all([buildRenewalsWithAdjustments(), buildRequests()]).then(function() {
+
+                history.sort(function (a, b) {
+                    if (a.position > b.position) {
+                        return 1;
+                    }
+                    if (a.position < b.position) {
+                        return -1;
+                    }
+                    return 0;
+                });
+
+                var graph = [];
+                var current_quantity = 0;
+                history.forEach(function(element) {
+                    current_quantity += element.add;
+                    graph.push([element.position, current_quantity]);
+                });
+
+                next(graph);
+            });
+        }
+
+
+
 
 
         /**
@@ -212,8 +226,11 @@ define(['q', 'async'], function(Q, async) {
 
                 async.each(beneficiary.renewals, function(r, nextRenewal) {
 
+                    r.paneltype = 'default';
+
                     if (r.start < now && r.finish > now) {
                         $scope.activePanel = panel;
+                        r.paneltype = 'primary';
                     }
 
                     panel++;
---
+++ public/js/services/decimalAdjust.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Decimal adjustment of a number.
+ *
+ * @param {String}  type  The type of adjustment.
+ * @param {Number}  value The number.
+ * @param {Integer} exp   The exponent (the 10 logarithm of the adjustment base).
+ * @returns {Number} The adjusted value.
+ */
+exports = module.exports = function decimalAdjust(type, value, exp) {
+    // If the exp is undefined or zero...
+    if (typeof exp === 'undefined' || +exp === 0) {
+        return Math[type](value);
+    }
+    value = +value;
+    exp = +exp;
+    // If the value is not a number or the exp is not an integer...
+    if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+        return NaN;
+    }
+    // Shift
+    value = value.toString().split('e');
+    value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+    // Shift back
+    value = value.toString().split('e');
+    return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+};
