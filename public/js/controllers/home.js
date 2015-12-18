7171459ab567d463f1bc9239c76fa2ab167b07c3
Paul de Rosanbo <paul@rosanbo.com>
2015-12-18 09:44:10 +0100

    collaborators vue

--- public/js/controllers/home.js
+++ public/js/controllers/home.js
@@ -17,11 +17,53 @@ define([], function() {
             var collaboratorsResource = Rest.account.collaborators.getResource();
 
             var endDate = new Date();
-            endDate.setDate(endDate.getDate() + 7);
+            var nbdays = 14;
+            endDate.setDate(endDate.getDate() + nbdays);
 
             $scope.collaborators = collaboratorsResource.query({
                 dtstart: new Date(),
                 dtend: endDate
+            });
+
+
+
+
+            $scope.collaborators.$promise.then(function(collaborators) {
+
+                collaborators.forEach(function(collaborator) {
+
+                    collaborator.days = {};
+                    var loopDate = new Date();
+
+                    loopDate.setHours(0,0,0,0);
+
+                    for (var d=0; d<nbdays; d++) {
+
+                        collaborator.days[loopDate.getTime()] = {
+                            date: new Date(loopDate),
+                            freebusy: false
+                        };
+
+
+                        loopDate.setDate(loopDate.getDate()+1);
+                    }
+
+                    var event, pos;
+
+                    for(var f=0; f<collaborator.freebusy.length; f++) {
+                        event = collaborator.freebusy[f];
+                        pos = new Date(event.dtstart);
+                        pos.setHours(0,0,0,0);
+
+                        if (undefined === collaborator.days[pos.getTime()]) {
+                            continue;
+                        }
+
+                        collaborator.days[pos.getTime()].freebusy = true;
+                    }
+
+
+                });
             });
         }
 
