6b578f01dc4234d75f3e0acbc095d5f9335e4c63
Paul de Rosanbo <paul@rosanbo.com>
2016-02-28 18:07:00 +0100

    Promisify two type of export

--- api/services/admin/exports/get.js
+++ api/services/admin/exports/get.js
@@ -1,9 +1,53 @@
 'use strict';
 
-var xlsx = require('xlsx-writestream');
-var Gettext = require('node-gettext');
-var gt = new Gettext();
-var tmp = require('tmp');
+let xlsx = require('xlsx-writestream');
+let Gettext = require('node-gettext');
+let gt = new Gettext();
+let tmp = require('tmp');
+
+
+/**
+ * Get data for each export type
+ */
+let exportTypes = {
+
+    /**
+     * Get balance on date
+     * @param {Object} params
+     * @returns {Promise}
+     */
+    balance: function(params) {
+
+        return new Promise(function(resolve, reject) {
+            if (undefined === params.moment) {
+                return reject(gt.gettext('moment is a mandatory parameter'));
+            }
+
+            resolve([]);
+        });
+
+
+    },
+
+    /**
+     * All requests between two dates
+     * @param {Object} params
+     * @returns {Promise}
+     */
+    requests: function(params) {
+
+        return new Promise(function(resolve, reject) {
+
+            if (undefined === params.from || undefined === params.to) {
+                return reject(gt.gettext('from and to are mandatory parameters'));
+            }
+
+            resolve([]);
+        });
+    }
+};
+
+
 
 
 exports = module.exports = function(services, app) {
@@ -18,38 +62,41 @@ exports = module.exports = function(services, app) {
      */
     service.getResultPromise = function(params) {
 
-        if (undefined === params.from || undefined === params.to) {
-            return service.error(gt.gettext('from and to are mandatory parameters'));
+
+        let format = 'xlsx';
+
+        if (undefined !== params.format && -1 !== ['xlsx', 'csv'].indexOf(params.format)) {
+            format = params.format;
         }
 
-        let type = 'xlsx';
-
-        if (undefined !== params.type && -1 !== ['xlsx', 'csv'].indexOf(params.type)) {
+        let type = 'balance';
+        if (undefined !== params.type && -1 !== ['balance', 'requests'].indexOf(params.type)) {
             type = params.type;
         }
 
 
-        let data = [];
+        exportTypes[type](params).then(function(data) {
 
-        let tmpname = tmp.tmpNameSync();
-        xlsx.write(tmpname, data, function (err) {
-            if (err) {
-                return service.deferred.reject(err);
-            }
-
-            service.res.download(tmpname, 'export.xlsx', function(err){
+            let tmpname = tmp.tmpNameSync();
+            xlsx.write(tmpname, data, function (err) {
                 if (err) {
-                    // Handle error, but keep in mind the response may be partially-sent
-                    // so check res.headersSent
-                    service.deferred.reject(err);
-
-                } else {
-                    // do not return json after download
-                    service.deferred.resolve(null);
+                    return service.deferred.reject(err);
                 }
-            });
-        });
 
+                service.res.download(tmpname, 'export.xlsx', function(err){
+                    if (err) {
+                        // Handle error, but keep in mind the response may be partially-sent
+                        // so check res.headersSent
+                        service.deferred.reject(err);
+
+                    } else {
+                        // do not return json after download
+                        service.deferred.resolve(null);
+                    }
+                });
+            });
+
+        }).catch(service.error);
 
 
         return service.deferred.promise;
