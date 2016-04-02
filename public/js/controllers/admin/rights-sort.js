a44d592263d7e724834efc966edabc14cc1f754b
Paul de Rosanbo <paul@rosanbo.com>
2016-04-02 17:59:26 +0200

    New sort interface for rights and types

--- public/js/controllers/admin/rights-sort.js
+++ public/js/controllers/admin/rights-sort.js
@@ -4,59 +4,91 @@ define([], function() {
 		'$scope',
 		'Rest',
         'gettext',
-        '$q',
-		function($scope, Rest, gettext, $q) {
+		function($scope, Rest, gettext) {
 
             $scope.setPageTitle(gettext('Sort absence rights'));
 
             var rightResource = Rest.admin.rights.getResource();
             var typeResource = Rest.admin.types.getResource();
 
-            $scope.rights = rightResource.query({});
+            var allrights = rightResource.query({});
             $scope.types = typeResource.query({});
+            $scope.rights = [];
 
-            $scope.sortableItems = [];
+            var selectedType = null;
 
-            $q.all([$scope.rights.$promise, $scope.types.$promise]).then(function(a) {
-                var rights = a[0];
-                var types = a[1];
 
-                // collect types acting as folded groups
+            /**
+             * Get the class for the type sortable item
+             * @param   {object} type Type in sortable item
+             * @returns {string} CSS class
+             */
+            $scope.getClass = function(type) {
+                if (null !== selectedType && selectedType._id === type._id) {
+                    return 'bg-primary';
+                }
 
-                types.forEach(function(type) {
-                    if (type.group.groupFolded) {
-                        $scope.sortableItems.push({
-                            name: type.groupTitle || type.name,
-                            object: type
-                        });
+                return null;
+            };
+
+
+            /**
+             * Click on the sort rights button
+             * @param   {object}   type
+             *
+             */
+            $scope.setRights = function(type) {
+                selectedType = type;
+                $scope.rights = allrights.filter(function(r) {
+                    return type._id === r.type._id;
+                });
+
+            };
+
+
+
+            /**
+             * Display sort rights button only if more than one right to sort
+             * @param   {object}  type
+             * @returns {boolean}
+             */
+            $scope.canSort = function(type) {
+                var c=0;
+                for(var i=0; i<allrights.length; i++) {
+                    if (allrights[i].type._id === type._id) {
+                        c++;
+                        if (c > 1) {
+                            return true;
+                        }
                     }
-                });
+                }
 
-                // collect rights ignoring folded
+                return false;
+            };
 
-                rights.forEach(function(right) {
-                    if (!right.type.groupFolded) {
-                        $scope.sortableItems.push({
-                            name: right.name,
-                            object: right
-                        });
-                    }
-                });
 
-                $scope.sortableItems.sort(function(s1, s2) {
-                    return s1.object.sortkey > s2.object.sortkey;
-                });
 
-            });
-
-            $scope.dragControlOptions = {
+            $scope.dragTypes = {
                 orderChanged: function() {
+                    /*
                     var pos = 1;
                     $scope.sortableItems.forEach(function(item) {
                         item.object.sortkey = pos++;
                         item.object.$save();
                     });
+                    */
+                }
+            };
 
+            $scope.dragRights = {
+                orderChanged: function() {
+                    /*
+                    var pos = 1;
+                    $scope.sortableItems.forEach(function(item) {
+                        item.object.sortkey = pos++;
+                        item.object.$save();
+                    });
+                    */
                 }
             };
 		}
