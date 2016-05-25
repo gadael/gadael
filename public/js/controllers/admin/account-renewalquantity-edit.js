109ca26e425586f83be953c5dd567cf384163818
Paul de Rosanbo <paul@rosanbo.com>
2016-05-25 17:52:15 +0200

    html & css

--- public/js/controllers/admin/account-renewalquantity-edit.js
+++ public/js/controllers/admin/account-renewalquantity-edit.js
@@ -7,7 +7,7 @@ define([], function() {
      * @param {Array} accountCollections
      * @return {Object}
      */
-    function getCollections(accountCollections) {
+    function getCollections(accountCollections, refDate) {
 
         var r = {
             prev: null,
@@ -15,10 +15,12 @@ define([], function() {
             next: null
         };
 
-        var today = new Date();
+        if (!refDate) {
+            refDate = new Date();
+        }
 
         for (var i=0; i<accountCollections.length; i++) {
-            if (accountCollections[i].from < today && accountCollections[i].to > today) {
+            if (accountCollections[i].from < refDate && accountCollections[i].to > refDate) {
 
                 if (i > 0) {
                     r.prev = accountCollections[i-1];
@@ -56,20 +58,52 @@ define([], function() {
 		) {
 
         var accountCollectionResource = Rest.admin.accountcollections.getResource();
+        var accoutBeneficiariesResource = Rest.admin.accountbeneficiaries.getResource();
+        var account, accountCollections;
 
 		$scope.user = Rest.admin.users.getFromUrl().loadRouteId();
 
+        function setAccountCollection(accountCollections, refDate) {
+            $scope.accountCollections = getCollections(accountCollections, refDate);
+            var collection = $scope.accountCollections.current;
+            var accoutBeneficiaries = accoutBeneficiariesResource.query({ account: account._id });
+
+            accoutBeneficiaries.$promise.then(function() {
+                $scope.beneficiaries = [];
+                var today = new Date();
+                accoutBeneficiaries.forEach(function(b) {
+
+                    for(var r = b.renewals.length; r--;) {
+                        if (b.renewals[r].start > collection.to || b.renewals[r].finish < collection.from) {
+                            // filter out renewals out of collection period
+                            b.renewals.splice(r, 1);
+                        }
+
+                        if (b.renewals[r].finish < today && b.renewals[r].available_quantity <= 0) {
+                            // filter out finished renewals with no remaining quantity
+                            b.renewals.splice(r, 1);
+                        }
+                    }
+
+                    if (0 === b.renewals.length) {
+                        // do not display rights with no renewals
+                        return;
+                    }
+
+                    $scope.beneficiaries.push(b);
+                });
+            });
+        }
 
         if ($scope.user.$promise) {
             $scope.user.$promise.then(function() {
                 if (undefined !== $scope.user.roles && $scope.user.roles.account && $scope.user.roles.account._id) {
 
-                    var account = $scope.user.roles.account;
-                    var accountCollections = accountCollectionResource.query({ account: account._id });
+                    account = $scope.user.roles.account;
+                    accountCollections = accountCollectionResource.query({ account: account._id });
 
                     accountCollections.$promise.then(function() {
-                        $scope.accountCollections = getCollections(accountCollections);
-                        console.log($scope.accountCollections);
+                        setAccountCollection(accountCollections);
                     });
                 }
 
@@ -77,7 +111,26 @@ define([], function() {
         }
 
 
+        /**
+         * Previous account-collection
+         */
+        $scope.prev = function() {
+            var refDate = new Date($scope.accountCollections.prev.from);
+            refDate.setDate(refDate.getDate()+1);
+            setAccountCollection(accountCollections, refDate);
+        };
 
+        /**
+         * Next account-collection
+         */
+        $scope.next = function() {
+            setAccountCollection(accountCollections, $scope.accountCollections.next.from);
+        };
+
+
+		/**
+		 * Get back to user view
+		 */
 		$scope.cancel = function() {
 			$location.path('/admin/users/'+$scope.user._id);
 		};
