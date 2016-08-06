74e7f8f481ccfae55261cc06a942286d943f48e5
Paul de Rosanbo <paul@rosanbo.com>
2016-08-06 20:55:59 +0200

    check requirements in form

--- public/js/controllers/admin/export/export-edit-sage.js
+++ public/js/controllers/admin/export/export-edit-sage.js
@@ -20,13 +20,35 @@ define([], function() {
             $scope.downloadUrl = null;
         });
 
+
+        /**
+         * Redirect user to the download link
+         */
         $scope.download = function() {
 
             var parameters = [];
 
+            if (!$scope.period.from || !$scope.period.to) {
+                return;
+            }
+
+            var typeSelected = false;
+            for (var i=0; i< $scope.types.length; i++) {
+                if ($scope.types[i].checked) {
+                    parameters.push('rightType[]='+$scope.types[i]._id);
+                    typeSelected = true;
+                }
+            }
+
+            if (!typeSelected) {
+                alert(gettext('At least one type must be selected'));
+                return;
+            }
+
             parameters.push('type=sage');
             parameters.push('from='+$scope.period.from.toJSON());
             parameters.push('to='+$scope.period.to.toJSON());
+
 
             $scope.downloadUrl = '/rest/admin/export?'+parameters.join('&');
             $timeout(function() {
