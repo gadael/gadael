6df3a1541fb5b2430ca6b25ef5d0c5f9526b9e87
Paul de Rosanbo <paul@rosanbo.com>
2016-08-06 22:32:10 +0200

    Add todo

--- rest/admin/export.js
+++ rest/admin/export.js
@@ -34,6 +34,7 @@ function getController() {
         promise.then(tmpname => {
 
             if ('sage' === params.type) {
+                //TODO: Declare encoding in header
                 ctrl.res.download(tmpname, 'export.sage', function() {
                     deleteTmpfile(tmpname);
                 });
