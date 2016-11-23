36a8e18b330585d31cca779901632f099e0956c6
Paul de Rosanbo <paul@rosanbo.com>
2016-11-23 20:31:51 +0100

    Fix app start

--- rest/anonymous/formlogin.js
+++ rest/anonymous/formlogin.js
@@ -11,21 +11,23 @@ const abuseFilter = require('../../modules/abusefilter');
 function createController() {
     // TODO: change to /rest/annonymous/formlogin
     ctrlFactory.create.call(this, '/rest/login');
+
     let controller = this;
-    let gt = controller.req.app.utility.gettext;
-    let workflow = controller.req.app.utility.workflow(controller.req, controller.res);
-
-
-    function validate() {
-        // TODO: rework this as Promise
-        if (workflow.needRequiredFields(['username', 'password'])) {
-            return workflow.emit('response');
-        }
-
-        abuseFilter(controller.req);
-    }
 
     this.controllerAction = function() {
+
+        let workflow = controller.req.app.utility.workflow(controller.req, controller.res);
+
+
+        function validate() {
+            // TODO: rework this as Promise
+            if (workflow.needRequiredFields(['username', 'password'])) {
+                return workflow.emit('response');
+            }
+
+            abuseFilter(controller.req);
+        }
+
 
         validate();
     };
