4696819fedc2e5d818e4ca6242e995ecc93933cd
Paul de Rosanbo <paul@rosanbo.com>
2017-01-28 14:31:27 +0100

    Fix non working days loading

--- public/js/services/departmentDays.js
+++ public/js/services/departmentDays.js
@@ -3,6 +3,55 @@ define(function() {
     'use strict';
 
 
+    function createCollaboratorDays(collaborator, startDate, nbdays) {
+        collaborator.days = {};
+        var loopDate = new Date(startDate);
+
+        loopDate.setHours(0,0,0,0);
+
+        for (var d=0; d<nbdays; d++) {
+
+            collaborator.days[loopDate.getTime()] = {
+                date: new Date(loopDate),
+                free: false,
+                events: [],
+                nonworkingday: null
+            };
+
+
+            loopDate.setDate(loopDate.getDate()+1);
+        }
+
+        var event, pos;
+
+
+
+        for (var f=0; f<collaborator.free.length; f++) {
+            event = collaborator.free[f];
+            pos = new Date(event.dtstart);
+            pos.setHours(0,0,0,0);
+
+            if (undefined === collaborator.days[pos.getTime()]) {
+                continue;
+            }
+
+            collaborator.days[pos.getTime()].free = true;
+        }
+
+        for (var e=0; e<collaborator.events.length; e++) {
+            event = collaborator.events[e];
+            pos = new Date(event.dtstart);
+            pos.setHours(0,0,0,0);
+
+            if (undefined === collaborator.days[pos.getTime()]) {
+                continue;
+            }
+
+            collaborator.days[pos.getTime()].events.push(event);
+        }
+    }
+
+
 
     /**
      * Prepare scope variables for departments visualization
@@ -42,13 +91,10 @@ define(function() {
 
             scopeOutput.adminLink = adminLink;
 
-            var promises = [];
-
             if (undefined !== collaboratorsResource) {
                 scopeOutput.collaborators = collaboratorsResource.query(collaboratorsParams);
-                promises.push(scopeOutput.collaborators.$promise);
             } else {
-                promises.push($q.resolve([]));
+                throw new Error('Missing collaborators resource');
             }
 
             scopeOutput.viewCollaborator = function(collaborator) {
@@ -59,86 +105,62 @@ define(function() {
                 $location.path('/admin/users/'+collaborator._id);
             };
 
-            var nonworkingdaysQuery = calendareventsResource.query({
-                type: 'nonworkingday',
-                dtstart: startDate,
-                dtend: endDate
-            });
-
-            promises.push(nonworkingdaysQuery.$promise);
 
 
-            return $q.all(promises)
-            .then(function(results) {
 
-                var collaborators = results[0];
-                var nonworkingdays = results[1];
+            return scopeOutput.collaborators.$promise
+            .then(function(collaborators) {
+
+                var nwdPromises = [];
 
                 for(var c=0; c<collaborators.length; c++) {
 
                     var collaborator = collaborators[c];
 
-                    collaborator.days = {};
-                    var loopDate = new Date(startDate);
+                    createCollaboratorDays(collaborator, startDate, nbdays);
 
-                    loopDate.setHours(0,0,0,0);
+                    
+                    var nonworkingdaysQuery = calendareventsResource.query({
+                        type: 'nonworkingday',
+                        dtstart: startDate,
+                        dtend: endDate,
+                        user: collaborator._id
+                    });
 
-                    for (var d=0; d<nbdays; d++) {
+                    nwdPromises.push(nonworkingdaysQuery.$promise);
+                }
 
-                        collaborator.days[loopDate.getTime()] = {
-                            date: new Date(loopDate),
-                            free: false,
-                            events: [],
-                            nonworkingday: null
-                        };
-
-
-                        loopDate.setDate(loopDate.getDate()+1);
-                    }
+                return $q.all(nwdPromises)
+                .then(function(nwdResults) {
 
                     var event, pos;
 
+                    for(var c=0; c<collaborators.length; c++) {
 
+                        var collaborator = collaborators[c];
+                        var nonworkingdays = nwdResults[c];
 
-                    for (var f=0; f<collaborator.free.length; f++) {
-                        event = collaborator.free[f];
-                        pos = new Date(event.dtstart);
-                        pos.setHours(0,0,0,0);
+                        for (var n=0; n<nonworkingdays.length; n++) {
+                            event = nonworkingdays[n];
+                            pos = new Date(event.dtstart);
+                            pos.setHours(0,0,0,0);
 
-                        if (undefined === collaborator.days[pos.getTime()]) {
-                            continue;
+                            if (undefined === collaborator.days[pos.getTime()]) {
+                                continue;
+                            }
+
+                            collaborator.days[pos.getTime()].free = false;
+                            collaborator.days[pos.getTime()].nonworkingday = event;
                         }
 
-                        collaborator.days[pos.getTime()].free = true;
                     }
 
-                    for (var e=0; e<collaborator.events.length; e++) {
-                        event = collaborator.events[e];
-                        pos = new Date(event.dtstart);
-                        pos.setHours(0,0,0,0);
+                    return collaborators;
 
-                        if (undefined === collaborator.days[pos.getTime()]) {
-                            continue;
-                        }
+                });
+            })
 
-                        collaborator.days[pos.getTime()].events.push(event);
-                    }
-
-
-                    for (var n=0; n<nonworkingdays.length; n++) {
-                        event = nonworkingdays[n];
-                        pos = new Date(event.dtstart);
-                        pos.setHours(0,0,0,0);
-
-                        if (undefined === collaborator.days[pos.getTime()]) {
-                            continue;
-                        }
-
-                        collaborator.days[pos.getTime()].free = false;
-                        collaborator.days[pos.getTime()].nonworkingday = event;
-                    }
-                }
-
+            .then(function() {
                 return scopeOutput;
             });
 
