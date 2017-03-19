370693d67371fb2f3dc2680260afa6d8a282b24b
Paul de Rosanbo <paul@rosanbo.com>
2017-03-19 09:27:49 +0100

    Delete button

--- public/js/controllers/admin/invitations.js
+++ public/js/controllers/admin/invitations.js
@@ -3,10 +3,21 @@ define([], function() {
     return [
 		'$scope',
         'gettext',
-		function($scope, gettext) {
+        '$http',
+        'catchOutcome',
+        '$route',
+		function($scope, gettext, $http, catchOutcome, $route) {
 
             $scope.setPageTitle(gettext('Invitations'));
 
+            $scope.delete = function(invitation) {
+                if (!confirm(gettext('Do you really want to delete this invitation?'))) {
+                    return false;
+                }
+
+                catchOutcome($http.delete('/rest/admin/invitations/'+invitation._id))
+                .then($route.reload);
+            };
 		}
 	];
 });
