6c92fc208a286e8c6888f9cf9ec9c0700f0f6b73
Paul de Rosanbo <paul@rosanbo.com>
2017-06-02 08:27:24 +0200

    filter out hidden rights

--- public/js/controllers/account/beneficiaries.js
+++ public/js/controllers/account/beneficiaries.js
@@ -13,6 +13,14 @@ define([], function() {
             var beneficiaries = Rest.account.beneficiaries.getResource();
             $scope.beneficiaries = beneficiaries.query();
 
+            $scope.beneficiaries.$promise.then(function() {
+                // filter out hidden rights
+
+                $scope.beneficiaries = $scope.beneficiaries.filter(function(b) {
+                    return !b.right.hide;
+                });
+            });
+
             $scope.getCurrentPeriod = function(renewals) {
                 var renewal, today = new Date();
                 for(var i=0; i<renewals.length; i++) {
