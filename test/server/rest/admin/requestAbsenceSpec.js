34771566a429b004ed279e0d4cf3187cbcb9a252
Paul de Rosanbo <paul@rosanbo.com>
2017-07-08 13:28:11 +0200

    Fix test

--- test/server/rest/admin/requestAbsenceSpec.js
+++ test/server/rest/admin/requestAbsenceSpec.js
@@ -493,7 +493,6 @@ describe('request absence admin rest service', function() {
     it('get request 1 when deleted', function(done) {
         server.get('/rest/admin/requests/'+request1._id, { 'status.deleted': 'accepted' }, function(res, body) {
             expect(res.statusCode).toEqual(200);
-            expect(body.$outcome).toBeDefined();
             done();
         });
     });
