2a31b628d5b2f0214a8988d249efc4bf16ed9506
Paul de Rosanbo <paul@rosanbo.com>
2019-12-14 16:09:59 +0100

    Add refresh button for last month

--- public/js/controllers/admin/lunchs.js
+++ public/js/controllers/admin/lunchs.js
@@ -2,14 +2,21 @@ define([], function() {
 
     'use strict';
 
-	return ['$scope', '$location', 'gettext', 'Rest',
-    function($scope, $location, gettext, Rest) {
+	return ['$scope', '$location', '$route', '$http', 'gettext', 'Rest', 'catchOutcome',
+    function($scope, $location, $route, $http, gettext, Rest, catchOutcome) {
 
         $scope.setPageTitle(gettext('Lunch breaks by months'));
         $scope.user = Rest.admin.users.getFromUrl().loadRouteId();
 
-        $scope.refresh = function() {
-            
+        $scope.refresh = function(d) {
+            catchOutcome(
+                $http.post('rest/admin/lunchs', {
+                    'user.id': $scope.user._id,
+                    year: d.getFullYear(),
+                    month: d.getMonth()
+    			})
+            )
+			.then($route.reload);
         };
 	}];
 });
