1954edbb2481627f8a5d31606d92e886433c4a11
Paul de Rosanbo <paul@rosanbo.com>
2020-01-26 18:45:13 +0100

    Add the UI

--- public/js/controllers/admin/account-overtimes-edit.js
+++ public/js/controllers/admin/account-overtimes-edit.js
@@ -10,6 +10,11 @@ define([], function() {
 		) {
 
 		$scope.user = Rest.admin.users.getFromUrl().loadRouteId();
+        $scope.overtimes = [];
+
+        $scope.addOvertime = function() {
+            $scope.overtimes.push({});
+        };
 
 		$scope.cancel = function() {
 			$location.path('/admin/users/'+$scope.user._id);
