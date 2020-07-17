3e60daf688e0002888f7624b56a304d018aef8d2
Paul de Rosanbo <paul@rosanbo.com>
2020-07-17 08:22:47 +0200

    Fix Error: $rootScope:inprog Action Already In Progress

--- public/js/controllers/admin/calendar-edit.js
+++ public/js/controllers/admin/calendar-edit.js
@@ -3,27 +3,24 @@ define([], function() {
     'use strict';
 
 	return ['$scope', '$location', 'Rest', function($scope, $location, Rest) {
-		
-		
+
+
 		$scope.calendar = Rest.admin.calendars.getFromUrl().loadRouteId();
 
 		if (!$scope.calendar.$promise)
 		{
-			
 			// no promise, new instance
 			$scope.calendar.type = 'workschedule';
             $scope.calendar.halfDayHour = new Date(null, null, null, 12, 0);
-			$scope.$apply();
 		}
-		
+
 		$scope.back = function() {
 			$location.path('/admin/calendars');
 		};
-		
+
 		$scope.saveCalendar = function() {
 			$scope.calendar.gadaSave($scope.back);
 	    };
-	    
+
 	}];
 });
-
